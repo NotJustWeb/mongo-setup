0ecb41b62b1e3999a92579c11934db505473dcad
Sanjay Adhikari <culblank.minded@gmail.com>
2022-11-27 23:07:48 +1100

    killing mongodb if exists

--- database-start.sh
+++ database-start.sh
@@ -3,5 +3,5 @@ source database-config.sh
 
 cd "$database_dir/bin"
 pwd
-kill $(lsof -t -i:27017) #kill if mongodb is already running
+kill -2 `pgrep mongo` #kill if mongodb is already running
 ./mongod --dbpath $data_path &
