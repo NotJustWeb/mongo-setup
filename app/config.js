010ed995da8ed3d99fb8d933a63c2d43839acea0
Sanjay Adhikari <culblank.minded@gmail.com>
2022-11-28 11:03:46 +1100

    Database connection example

---
+++ app/config.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
--- mongo.js
+++ mongo.js
@@ -1,9 +1,22 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /listings.
+const appRoutes = express.Router();
+
+// This section will help you get a list of all the documents.
+appRoutes.route("/").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+  
+    dbConnect
+      .collection("test")
+      .find({}).limit(50)
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).json({ msg:"Error fetching test database!", success:false, err:err });
+       } else {
+          res.json({ success:true, msg:"Successfully got the result.", result:result });
+        }
+      });
+  });
