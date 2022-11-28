0daac564c50f9c32bab727168a8822e0563012d6
Sanjay Adhikari <culblank.minded@gmail.com>
2022-11-28 11:31:30 +1100

    Connected to the server

--- mongo.js
+++ mongo.js
@@ -1,22 +1,48 @@
+var http = require('http');
 const express = require("express");
 
+// get MongoDB driver connection
+const dbo = require('./app/connection');
+
 // recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
-const appRoutes = express.Router();
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+// Global error handling
+app.use(function (err, _req, res) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+const server = http.createServer(app);
+
+// perform a database connection when the server starts
+dbo.connectToServer(function (err) {
+    if (err) {
+      console.error(err);
+      process.exit();
+    }
+  
+    console.log("Connected to the database");
+
+    server.listen();
+});
 
 // This section will help you get a list of all the documents.
-appRoutes.route("/").get(async function (req, res) {
-    const dbConnect = dbo.getDb();
-  
-    dbConnect
-      .collection("test")
-      .find({}).limit(50)
-      .toArray(function (err, result) {
+app.route("/").get(async function (req, res) {
+const dbConnect = dbo.getDb();
+
+dbConnect
+    .collection("test")
+    .find({}).limit(50)
+    .toArray(function (err, result) {
         if (err) {
-          res.status(400).json({ msg:"Error fetching test database!", success:false, err:err });
-       } else {
-          res.json({ success:true, msg:"Successfully got the result.", result:result });
+            res.status(400).json({ msg:"Error fetching test database!", success:false, err:err });
+        } else {
+            res.json({ success:true, msg:"Successfully got the result.", result:result });
         }
-      });
-  });
+    });
+});
